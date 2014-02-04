28f59515aea34d7158b81ee515b0d1470d828777
yann <pradayrol.yannick@gmail.com>
2014-02-04 17:48:48 +0100

    minor changes
    still a draft

--- insert.py
+++ insert.py
@@ -16,6 +16,7 @@ Usage:
     sys.exit()
 
 uid = 1
+host = 'http://172.16.1.73:8069'
 dbname = sys.argv[2]
 password = sys.argv[3]
 input_name = sys.argv[1]
@@ -26,14 +27,22 @@ input_kwargs = {
     'lineterminator': '\n',
 }
 
-sock = xmlrpclib.ServerProxy('http://172.16.1.73:8069/xmlrpc/object')
+sock = xmlrpclib.ServerProxy(host + '/xmlrpc/object')
 
 res_partner_title_ids = sock.execute(dbname, uid, password, 'res.partner.title', 'search', [])
 res_country_ids = sock.execute(dbname, uid, password, 'res.country', 'search', [])
 
-data = {
-    'title': sock.execute(dbname, uid, password, 'res.partner.title', 'read', res_partner_title_ids, []),
-    'country': sock.execute(dbname, uid, password, 'res.country', 'read', res_country_ids, []),
+relationnal_data = {
+    'title': {
+        'table_name': 'res.partner.title',
+        'column_name': 'title',
+        'data': sock.execute(dbname, uid, password, 'res.partner.title', 'read', res_partner_title_ids, []),
+    },
+    'country': {
+        'table_name': 'res.country',
+        'column_name': 'country_id',
+        'data': sock.execute(dbname, uid, password, 'res.country', 'read', res_country_ids, []),
+    },
 }
 
 # names of columns must be exactly the same as those defined here (edit here if necessary, but be sure to edit data as well)
@@ -42,13 +51,14 @@ keys = ['ref','title','name','street','zip','city','country','phone','mobile','f
 
 def search_id_by_name(data, name):
     '''
-data: list of objects from the database
-name: value of the field 'name'
-return: id of the object which match the name provided
+data: list of objects
+name: value of the attribute 'name'
+return: id of the object which match the value of the attribute 'name' provided
     '''
     for item in data:
         if item['name'] == name:
             return item['id']
+    return None
 
 with open(input_name, 'r') as input_file:
     cr = csv.DictReader(input_file, **input_kwargs)
@@ -60,8 +70,13 @@ with open(input_name, 'r') as input_file:
         res_partner_data = {}
         for key in keys:
             # handle relationnal values
-            if key in data:
-                res_partner_data[key] = search_id_by_name(data[key], row[key]) or False
+            if key in relationnal_data:
+                some_id = search_id_by_name(relationnal_data[key]['data'], row[key])
+                if some_id is None:
+                    print('\nUnknow value "' + str(row[key]) + '" for attribute "name" in table "' + relationnal_data[key]['table_name'] + '"')
+                    res_partner_data[relationnal_data[key]['column_name']] = ''
+                else:
+                    res_partner_data[relationnal_data[key]['column_name']] = some_id
             else:
                 # handle boolean values quoted
                 if row[key] == 'True':
