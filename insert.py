64ffc03d84402f3546d8c8d69f9fdf4d92837b52
yann <pradayrol.yannick@gmail.com>
2014-02-05 17:45:00 +0100

    little
    changes to make the code more simple

--- insert.py
+++ insert.py
@@ -8,18 +8,22 @@ import csv
 
 beginning = time.time()
 
-if len(sys.argv) < 4:
+if len(sys.argv) < 5:
     print('''
 Usage:
-    python insert.py [csv file name] [database name] [password]
+    python insert.py [host] [database] [password] [file.csv]
 ''')
     sys.exit()
 
 uid = 1
-host = 'http://172.16.1.73:8069'
+host = sys.argv[1]
 dbname = sys.argv[2]
 password = sys.argv[3]
-input_name = sys.argv[1]
+input_name = sys.argv[4]
+
+
+# config ##################################
+
 input_kwargs = {
     'delimiter': ',',
     'quotechar': "'",
@@ -27,6 +31,12 @@ input_kwargs = {
     'lineterminator': '\n',
 }
 
+# names of columns must be exactly the same as those defined here (edit here if necessary, but be sure to edit data as well)
+keys = ['ref','title','name','street','zip','city','country','phone','mobile','fax','email','website','customer','is_company']
+
+###########################################
+
+# connect
 sock = xmlrpclib.ServerProxy(host + '/xmlrpc/object')
 
 # prepare the relationnal data
@@ -54,17 +64,16 @@ for key in relationnal_data:
     for item in data:
         relationnal_data[key]['data'][item['name']] = item['id']
 
-# names of columns must be exactly the same as those defined here (edit here if necessary, but be sure to edit data as well)
-keys = ['ref','title','name','street','zip','city','country','phone','mobile','fax','email','website','customer','is_company']
-
+def message(msg):
+    sys.stdout.write(msg)
+    sys.stdout.flush()
 
 with open(input_name, 'r') as input_file:
     cr = csv.DictReader(input_file, **input_kwargs)
     line = 0
     for row in cr:
         line += 1
-        sys.stdout.write(str(line) + ' Adding client: ' + row['name'] + ' ...')
-        sys.stdout.flush()
+        message(str(line) + ' Adding client: ' + row['name'] + ' ...')
         res_partner_data = {}
         for key in keys:
             value = row[key]
@@ -83,14 +92,10 @@ with open(input_name, 'r') as input_file:
                 res_partner_data[key] = value or False
         # insert values
         res_partner_id = sock.execute(dbname, uid, password, 'res.partner', 'create', res_partner_data)
-        sys.stdout.write('... done with id: ' + str(res_partner_id) + '\n')
-        sys.stdout.flush()
+        message('... done with id: ' + str(res_partner_id) + '\n')
 
 end = time.time()
 seconds = end - beginning
 
-print
-print(str(line) + ' clients added in ' + time.strftime('%H:%M:%S', time.gmtime(seconds))
-)
-print
+message('\n' + str(line) + ' clients added in ' + time.strftime('%H:%M:%S', time.gmtime(seconds)) + '\n')
 
